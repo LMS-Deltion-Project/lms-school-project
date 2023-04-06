4767205cc1c045d8605a360912d80dc95d9c6c66
ludotielbeke <ludotielbeke@gmail.com>
2023-04-06 09:30:08 +0200

    added menu

---
+++ services/web/components/menu/headerWrapper.vue
@@ -0,0 +1,30 @@
+<template>
+
+  <div v-if="!menu.state" class="xs:flex hidden w-full" @click="menu.state = !menu.state">
+    <Bars3Icon class="w-12 cursor-pointer"/>
+  </div>
+
+  <div v-if="menu.state">
+    <div class="z-10 w-screen h-screen left-0 top-0 overflow-hidden absolute" @click="menu.state = !menu.state">
+      <div class="h-[10%]">
+        <Header @menuState="() => {menu.state = !menu.state}"/>
+      </div>
+      <div class="h-[90%] w-full relative ">
+        <mobile-menu :menu="props.menuItems"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {Bars3Icon} from "@heroicons/vue/20/solid";
+import Header from "./header.vue";
+import MobileMenu from "~/components/menu/mobileMenu.vue";
+
+const menu = reactive({ state: false })
+
+const props = defineProps<{
+  menuItems: Array<object>
+}>()
+
+</script>
--- services/web/pages/index.vue
+++ services/web/pages/index.vue
@@ -3,11 +3,12 @@
         <div class="flex justify-between">
 
              <Logo styles="w-40" />
-            <div class="flex xs:hidden">
-                sdkfjksldfj
+            <div class="flex xs:hidden items-center underline">
+                <nuxt-link to="/browse" class="mx-6">Browse</nuxt-link>
+                <nuxt-link to="/login" class="mx-6">Login</nuxt-link>
             </div>
-            <div v-if="widthtest" class="h-full flex justify-center">
-                 <headerMenu @test="(menuState) => test(menuState)" />
+            <div v-if="screenSize.width < 600" class="h-full flex items-center">
+                 <headerMenu :menuItems="menuItems"/>
             </div>
         </div>
         <div class="w-96 text-white md:pl-24 sm:pt-28 xs:w-full xs:h-1/3 flex flex-col xs:justify-end">
@@ -18,7 +19,7 @@
                 Start creating your own course by creating an account
             </div>
             <div class="flex justify-center">
-                 <Btn text="Get started" extraStyles="mt-2 w-full xs:w-[75%] xs:text-center" link="/test">
+                 <Btn text="Get started" class="mt-2 w-full xs:w-[75%] xs:text-center" link="/test">
                      <ChevronRightIcon class="w-[22px] ml-2 inline-flex" />
                  </Btn>
             </div>
@@ -33,11 +34,12 @@
         </div>
     </div>
     <div class="w-full px-[10%] pt-9 flex items-center flex-col">
-         <Btn text="Browse" extraStyles="w-48 flex text-2xl xs:hidden" link="/browse">
+
+         <Btn text="Browse" class="w-48 flex text-2xl" link="/browse">
              <ChevronRightIcon class="w-[36px] ml-2 inline-flex " />
          </Btn>
 
-         <Btn text="Browse" link="/test">
+         <Btn text="Browse" class="w-48 flex text-2xl xs:hidden" link="/test">
              <ChevronRightIcon class="w-[36px] ml-2 inline-flex " />
          </Btn>
 
@@ -48,23 +50,23 @@
 <script setup lang="ts">
 import {ChevronDownIcon} from "@heroicons/vue/20/solid";
 import {ChevronRightIcon} from "@heroicons/vue/24/solid";
-import HeaderMenu from "~/components/menu/headerMenu.vue";
-import { defineCustomElement } from 'vue'
-import {computed, ref} from "vue";
+import HeaderMenu from "~/components/menu/headerWrapper.vue";
 
-// const widthtest = reactive({ width: window.innerWidth })
+const screenSize = reactive({ width: 0 })
 
-function test(menuState: Boolean) {
-  console.log(menuState)
+onMounted((): void => {
+  window.addEventListener('resize', () => handleWindowResize());
+  screenSize.width = document.body.clientWidth;
+})
+
+function handleWindowResize (): void {
+  screenSize.width = document.body.clientWidth
 }
 
-// const width = computed(() => {
-//   console.log(typeof document.body.clientWidth)
-//   return document.body.clientWidth
-// })
-
-watch(widthtest, (obj) => {
-  return obj.width
-})
+const menuItems = [
+  {text: "test", link: "/test"},
+  {text: "test", link: "/test"},
+  {text: "test", link: "/test"}
+]
 
 </script>
